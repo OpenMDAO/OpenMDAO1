d141655dce1c8980dcc2dba75ea291db43e34240
justin gray <justin.s.gray@gmail.com>
2015-04-18 22:42:08 -0400

    small name change from d_param to vec_param

--- sample_inputs/sellar_mdf.py
+++ sample_inputs/sellar_mdf.py
@@ -30,10 +30,10 @@ class SellarDis1(Component):
     #     pass
 
     #    pvec, uvec, rvec, dpvec, duvec, drvec
-    # def solve_linear(self, mode="fwd", params, unknowns, resids, dparams, dunknowns, dresids):
+    # def solve_linear(self, mode="fwd", params, unknowns, resids, vec_params, vec_unknowns, vec_resids):
     #     pass
 
-    # def apply_linear(self, mode="fwd", params, unknowns, resids, dparams, dunknowns, dresids):
+    # def apply_linear(self, mode="fwd", params, unknowns, resids, vec_params, vec_unknowns, vec_resids):
     #     pass
 
     # def preconditioner(self):
