92575ced4c0d41845e7671ebf6399645c36423c3
Kenneth Moore <kenneth.t.moore-1@nasa.gov>
2015-05-01 12:15:12 -0400

    take it from here, cal

--- openmdao/core/test/test_group_connections.py
+++ openmdao/core/test/test_group_connections.py
@@ -15,13 +15,25 @@ class TestGroupConnect(unittest.TestCase):
 
     def setUp(self):
         self.top = Problem(root=Group())
-        self.top.root.add('src', SimpleComp())
-        self.top.root.add('tgt', SimpleComp())
+        self.src = self.top.root.add('src', SimpleComp())
+        self.tgt = self.top.root.add('tgt', SimpleComp())
 
-    def test_connection_vartypes(self):
+    def test_connection_array_array(self):
 
         # Array to Array same size
-        self.top.root.src.add_output('y1', np.zeros((3, 1)))
+
+        self.src.add_output('y', np.zeros((2, 1)))
+        self.tgt.add_output('x', np.zeros((2, 1)))
+
+        self.top.root.connect('src:y', "tgt:x")
+
+        self.top.run()
+        var = self.top.varmanager.unknowns['tgt:y']
+        self.assertEqual(len(var), 2)
+        self.assertEqual(var[0][0], 0.0)
+        self.assertEqual(var[1][0], 0.0)
+
+
 
 if __name__ == "__main__":
     unittest.main()
