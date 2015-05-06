0921b8103372da512378f3fe0e6063015d52a904
Bret Naylor <naylor.b@gmail.com>
2015-05-06 17:04:25 -0400

    fixed indexing issue in assign_params

--- openmdao/core/problem.py
+++ openmdao/core/problem.py
@@ -1,5 +1,7 @@
 """ OpenMDAO Problem class defintion."""
 
+from six.moves import zip
+
 from openmdao.core.component import Component
 from openmdao.core.driver import Driver
 from openmdao.core.group import _get_implicit_connections
@@ -108,7 +110,7 @@ class Problem(Component):
 
 def assign_parameters(connections):
     """Map absolute system names to the absolute names of the
-    parameters they control
+    parameters they control.
     """
     param_owners = {}
 
@@ -117,10 +119,12 @@ def assign_parameters(connections):
         unk_parts = unk.split(':')
 
         common_parts = []
-        i = 0
-        while(par_parts[i] == unk_parts[i]):
-            common_parts.append(par_parts[i])
-            i = i+1
+        for ppart, upart in zip(par_parts, unk_parts):
+            if ppart == upart:
+                common_parts.append(ppart)
+            else:
+                break
+
         owner = ':'.join(common_parts)
 
         if owner in param_owners:
