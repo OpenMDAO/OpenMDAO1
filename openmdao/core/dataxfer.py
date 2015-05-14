c51d9862d1832703b8c02f1ae56116af44dbd72d
Kenneth Moore <kenneth.t.moore-1@nasa.gov>
2015-05-14 13:43:06 -0400

    fix for the problem, but is it a good fix?

--- openmdao/core/dataxfer.py
+++ openmdao/core/dataxfer.py
@@ -24,9 +24,25 @@ class DataXfer(object):
         they are connected to
     """
     def __init__(self, src_idxs, tgt_idxs, flat_conns, noflat_conns):
+
+        # We don't want any duplicate (src,tgt) pairs.
+        #TODO - Probably need to do this for the scatters too, so it might
+        #need to be at a higher level
+        new_src = []
+        new_tgt = []
+        seen_pairs = []
+        for src, tgt in zip(src_idxs, tgt_idxs):
+            if (src, tgt) not in seen_pairs:
+                new_src.append(src)
+                new_tgt.append(tgt)
+                seen_pairs.append((src, tgt))
+        src_idxs = new_src
+        tgt_idxs = new_tgt
+
         # TODO: change to_slice to to_slices. (should never return an index array)
         #self.src_idxs = to_slice(src_idxs)
         #self.tgt_idxs = to_slice(tgt_idxs)
+
         self.src_idxs = src_idxs
         self.tgt_idxs = tgt_idxs
         self.flat_conns = flat_conns
