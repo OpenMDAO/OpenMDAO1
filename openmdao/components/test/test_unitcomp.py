Corrected derivative for array valued input.

openmdao/components/test/test_unitcomp.py
@@ -58,7 +58,7 @@ class TestUnitComp(unittest.TestCase):
         prob.setup()
         prob.run()
 
-        assert_rel_error(self, prob['x_out'], 32., 1e-12)
+        assert_rel_error(self, prob['x_out'], 32., 1e-6)
 
         param_list = ['x']
         unknown_list = ['x_out']
@@ -72,3 +72,26 @@ class TestUnitComp(unittest.TestCase):
         J = prob.calc_gradient(param_list, unknown_list, mode='fwd',
                                return_format='dict')
         assert_rel_error(self, J['x_out']['x'][0][0], 1.8, 1e-6)
+
+    def test_array_values(self):
+        prob = Problem()
+        root = prob.root = Group()
+        root.add('pc', ParamComp('x', np.zeros((2,3)), units='degC'), promotes=['x'])
+        root.add('uc', UnitComp(shape=(2,3), param_name='x', out_name='x_out', units='degF'), promotes=['x', 'x_out'])
+        prob.setup()
+        prob.run()
+
+        assert_rel_error(self, prob['x_out'], np.array([[32., 32., 32.],[32., 32., 32.]]), 1e-6)
+
+        param_list = ['x']
+        unknown_list = ['x_out']
+
+        # Forward Mode
+        J = prob.calc_gradient(param_list, unknown_list, mode='fwd',
+                               return_format='dict')
+        assert_rel_error(self, J['x_out']['x'],1.8*np.eye(6), 1e-6)
+
+        # Reverse Mode
+        J = prob.calc_gradient(param_list, unknown_list, mode='fwd',
+                               return_format='dict')
+        assert_rel_error(self, J['x_out']['x'],1.8*np.eye(6), 1e-6)

openmdao/components/unitcomp.py
@@ -23,6 +23,7 @@ class UnitComp(Component):
 
         self.param_name = param_name
         self.out_name = out_name
+        self.shape = shape
 
         if param_name == out_name:
             msg = "UnitComp param_name cannot match out_name: '{name}'"
@@ -76,5 +77,5 @@ class UnitComp(Component):
             `VecWrapper`  containing residuals. (r)
         """
         J = {}
-        J[(self.out_name, self.param_name)] = np.array([1.0])
+        J[(self.out_name, self.param_name)] = np.eye(np.prod(self.shape))
         return J