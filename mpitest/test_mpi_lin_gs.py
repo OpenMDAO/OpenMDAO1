6de4bd2c3a7f4b71e5cfe7d2c1b8255b784f271f
Kenneth Moore <kenneth.t.moore-1@nasa.gov>
2015-06-19 10:02:54 -0400

    a problem still

--- mpitest/test_mpi_lin_gs.py
+++ mpitest/test_mpi_lin_gs.py
@@ -19,22 +19,22 @@ class MPITests1(MPITestCase):
 
     N_PROCS = 2
 
-    #def test_fan_out_grouped(self):
+    def test_fan_out_grouped(self):
 
-        #top = Problem(impl=impl)
-        #top.root = FanOutGrouped()
-        #top.root.ln_solver = LinearGaussSeidel()
-        #top.root.sub.ln_solver = LinearGaussSeidel()
-        #top.setup()
-        #top.run()
+        top = Problem(impl=impl)
+        top.root = FanOutGrouped()
+        top.root.ln_solver = LinearGaussSeidel()
+        top.root.sub.ln_solver = LinearGaussSeidel()
+        top.setup()
+        top.run()
 
-        #param_list = ['p.x']
-        #unknown_list = ['sub.comp2.y', "sub.comp3.y"]
+        param_list = ['p.x']
+        unknown_list = ['sub.comp2.y', "sub.comp3.y"]
 
-        #J = top.calc_gradient(param_list, unknown_list, mode='fwd', return_format='dict')
-        #print(J)
-        #assert_rel_error(self, J['sub.comp2.y']['p.x'][0][0], -6.0, 1e-6)
-        #assert_rel_error(self, J['sub.comp3.y']['p.x'][0][0], 15.0, 1e-6)
+        J = top.calc_gradient(param_list, unknown_list, mode='fwd', return_format='dict')
+        print(J)
+        assert_rel_error(self, J['sub.comp2.y']['p.x'][0][0], -6.0, 1e-6)
+        assert_rel_error(self, J['sub.comp3.y']['p.x'][0][0], 15.0, 1e-6)
 
         #J = top.calc_gradient(param_list, unknown_list, mode='rev', return_format='dict')
         #print(J)
@@ -58,10 +58,10 @@ class MPITests1(MPITestCase):
         assert_rel_error(self, J['comp3.y']['p1.x1'][0][0], -6.0, 1e-6)
         assert_rel_error(self, J['comp3.y']['p2.x2'][0][0], 35.0, 1e-6)
 
-        J = top.calc_gradient(param_list, unknown_list, mode='rev', return_format='dict')
-        print(J)
-        assert_rel_error(self, J['comp3.y']['p1.x1'][0][0], -6.0, 1e-6)
-        assert_rel_error(self, J['comp3.y']['p2.x2'][0][0], 35.0, 1e-6)
+        #J = top.calc_gradient(param_list, unknown_list, mode='rev', return_format='dict')
+        #print(J)
+        #assert_rel_error(self, J['comp3.y']['p1.x1'][0][0], -6.0, 1e-6)
+        #assert_rel_error(self, J['comp3.y']['p2.x2'][0][0], 35.0, 1e-6)
 
 if __name__ == '__main__':
     from openmdao.test.mpiunittest import mpirun_tests
