92b0f0827f93f9e7d71238d9eb62507757c17a8a
Drayton Munster <drayton.w.munster@nasa.gov>
2015-06-25 13:26:29 -0400

    Python 3 Compatibility

--- openmdao/core/test/test_indices.py
+++ openmdao/core/test/test_indices.py
@@ -20,23 +20,22 @@ class TestIndices(unittest.TestCase):
         root = prob.root
 
         root.add('P1', ParamComp('x', np.zeros(size)))
-        root.add('C1', ExecComp('y = x * 2.', y=np.zeros(size/2), x = np.zeros(size/2)))
-        root.add('C2', ExecComp('y = x * 3.', y=np.zeros(size/2), x = np.zeros(size/2)))
+        root.add('C1', ExecComp('y = x * 2.', y=np.zeros(size//2), x = np.zeros(size//2)))
+        root.add('C2', ExecComp('y = x * 3.', y=np.zeros(size//2), x = np.zeros(size//2)))
 
-        root.connect('P1.x', "C1.x", src_indices=list(range(size/2)))
-        root.connect('P1.x', "C2.x", src_indices=list(range(size/2, size)))
+        root.connect('P1.x', "C1.x", src_indices=list(range(size//2)))
+        root.connect('P1.x', "C2.x", src_indices=list(range(size//2, size)))
 
         prob.setup()
 
-        root.P1.unknowns['x'][0:size/2] += 1.0
-        root.P1.unknowns['x'][size/2:size] -= 1.0
+        root.P1.unknowns['x'][0:size//2] += 1.0
+        root.P1.unknowns['x'][size//2:size] -= 1.0
 
         prob.run()
 
-        assert_rel_error(self, root.C1.params['x'], np.ones(size/2), 0.0001)
-        assert_rel_error(self, root.C2.params['x'], -np.ones(size/2), 0.0001)
+        assert_rel_error(self, root.C1.params['x'], np.ones(size//2), 0.0001)
+        assert_rel_error(self, root.C2.params['x'], -np.ones(size//2), 0.0001)
 
 
 if __name__ == "__main__":
     unittest.main()
-
