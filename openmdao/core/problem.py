ddf8972b991872e6a57aa03a8ae772b90f9b7227
Bret Naylor <naylor.b@gmail.com>
2015-07-14 14:32:55 -0400

    added cycle detection and out-of-order detection to check_setup

--- openmdao/core/problem.py
+++ openmdao/core/problem.py
@@ -10,6 +10,7 @@ from collections import OrderedDict
 from itertools import chain
 from six import iteritems, string_types
 from six.moves import cStringIO
+import networkx as nx
 
 # pylint: disable=E0611, F0401
 import numpy as np
@@ -24,8 +25,9 @@ from openmdao.core.component import Component
 from openmdao.core.driver import Driver
 from openmdao.core.mpiwrap import MPI, FakeComm, under_mpirun
 from openmdao.core.relevance import Relevance
+from openmdao.solvers.run_once import RunOnce
 from openmdao.units.units import get_conversion_tuple
-from openmdao.util.strutil import get_common_ancestor
+from openmdao.util.strutil import get_common_ancestor, name_relative_to
 from openmdao.devtools.debug import debug
 
 
@@ -36,6 +38,7 @@ class Problem(System):
 
     def __init__(self, root=None, driver=None, impl=None):
         super(Problem, self).__init__()
+        self._setup_complete = False
         self.root = root
         if impl is None:
             self._impl = BasicImpl
@@ -155,6 +158,8 @@ class Problem(System):
         """Performs all setup of vector storage, data transfer, etc.,
         necessary to perform calculations.
         """
+        self._setup_complete = False
+
         # if we modify the system tree, we'll need to call _setup_variables
         # and _setup_connections again
         tree_changed = False
@@ -253,6 +258,8 @@ class Problem(System):
         # Prepare Driver
         self.driver._setup(self.root)
 
+        self._setup_complete = True
+
     def check_setup(self, out_stream=sys.stdout):
         """Write a report to the given stream indicating any potential problems found
         with the current configuration.
@@ -262,6 +269,9 @@ class Problem(System):
         out_stream : a file-like object
             Stream where report will be written.
         """
+        # make sure that setup() has been called before check_setup()
+        if not self._setup_complete:
+            raise RuntimeError("setup() must be called before check_setup()")
 
         # check for parameters that are not connected to a source/unknown.
         # this includes ALL dangling params, both promoted and unpromoted.
@@ -285,7 +295,7 @@ class Problem(System):
         # list all unit conversions being made (including only units on one side)
         if self._unit_diffs:
             print("\nUnit Conversions")
-            for (src,tgt), (sunit,tunit) in self._unit_diffs.items():
+            for (src,tgt), (sunit,tunit) in sorted(self._unit_diffs.items()):
                 print("%s -> %s : %s -> %s" % (src, tgt, sunit, tunit), file=out_stream)
 
         # Components without unknowns
@@ -308,7 +318,7 @@ class Problem(System):
 
         # No case recorder
         if not self.driver.recorders:
-            for grp in self.root.subgroups(recurse=True, local=True):
+            for grp in self.root.subgroups(recurse=True, local=True, include_self=True):
                 if grp.nl_solver.recorders or grp.ln_solver.recorders:
                     break
             else:
@@ -318,7 +328,7 @@ class Problem(System):
         if under_mpirun():
             # Indicate that there are no parallel systems if user is running under MPI
             if MPI.COMM_WORLD.rank == 0:
-                for grp in self.root.subgroups(recurse=True):
+                for grp in self.root.subgroups(recurse=True, include_self=True):
                     if isinstance(grp, ParallelGroup):
                         break
                 else:
@@ -331,17 +341,68 @@ class Problem(System):
                           (MPI.COMM_WORLD.size, maxcpu))
         # or any ParalleGroups found when not running under MPI
         else:
-            for grp in self.root.subgroups(recurse=True):
+            for grp in self.root.subgroups(recurse=True, include_self=True):
                 if isinstance(grp, ParallelGroup):
                     print("\nFound ParallelGroup '%s', but not running under MPI." %
                           grp.pathname, file=out_stream)
 
         # Cycles in group w/o solver
-        # Components/Systems/Groups are not in the right execution order
+        cgraph = self.root._relevance._cgraph
+        for grp in self.root.subgroups(recurse=True, include_self=True):
+            path = [] if not grp.pathname else grp.pathname.split('.')
+            graph = cgraph.subgraph([n for n in cgraph if n.startswith(grp.pathname)])
+            renames = {}
+            for node in graph.nodes_iter():
+                renames[node] = '.'.join(node.split('.')[:len(path)+1])
+                if renames[node] == node:
+                    del renames[node]
 
-        # Incomplete optimization driver configuration
-        # Parallelizability for users running serial models
-        # io state of recorder-specific files?
+            # get the graph of direct children of current group
+            nx.relabel_nodes(graph, renames, copy=False)
+
+            # remove self loops created by renaming
+            graph.remove_edges_from([(u,v) for u,v in graph.edges()
+                                         if u==v])
+
+            strong = [s for s in nx.strongly_connected_components(graph)
+                        if len(s)>1]
+
+            if strong and isinstance(grp.nl_solver, RunOnce): # no solver, cycles BAD
+                relstrong = []
+                for slist in strong:
+                    relstrong.append([])
+                    for s in slist:
+                        relstrong[-1].append(name_relative_to(grp.pathname, s))
+                print("Group '%s' has the following cycles: %s" %
+                     (grp.pathname, relstrong), file=out_stream)
+
+            # Components/Systems/Groups are not in the right execution order
+            subnames = [s.pathname for s in grp.subsystems()]
+            while strong:
+                # break cycles to check order
+                lsys = [s for s in subnames if s in strong[0]]
+                for p in graph.predecessors(lsys[0]):
+                    if p in lsys:
+                        graph.remove_edge(p, lsys[0])
+                strong = [s for s in nx.strongly_connected_components(graph)
+                            if len(s)>1]
+
+            visited = set()
+            out_of_order = set()
+            for sub in grp.subsystems():
+                visited.add(sub.pathname)
+                for u,v in nx.dfs_edges(graph, sub.pathname):
+                    if v in visited:
+                        out_of_order.add(v)
+
+            if out_of_order:
+                print("In group '%s', the following subsystems are out-of-order: %s" %
+                      (grp.pathname, sorted([name_relative_to(grp.pathname, n)
+                                                for n in out_of_order])), file=out_stream)
+
+        # TODO: Incomplete optimization driver configuration
+        # TODO: Parallelizability for users running serial models
+        # TODO: io state of recorder-specific files?
 
         # loop over subsystems and let them add any specific checks to the stream
         for s in self.root.subsystems(recurse=True, local=True, include_self=True):
