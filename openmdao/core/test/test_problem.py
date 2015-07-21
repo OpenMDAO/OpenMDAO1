b647f0f6c8d64281bdd4b9dd840cf7b1e54d96ff
Bret Naylor <naylor.b@gmail.com>
2015-07-21 15:09:21 -0400

    fix for different dict ordering on different platforms

--- openmdao/core/test/test_problem.py
+++ openmdao/core/test/test_problem.py
@@ -863,8 +863,10 @@ class TestCheckSetup(unittest.TestCase):
 
         stream = cStringIO()
         checks = prob.setup(out_stream=stream)
-        g = G2._get_sys_graph()
-        self.assertEqual(G2.list_auto_order(), ['C1', 'C3', 'C2'])
+        auto = G2.list_auto_order()
+        self.assertTrue(auto==['C1', 'C3', 'C2'] or
+                        auto==['C3', 'C2', 'C1'] or
+                        auto==['C2', 'C1', 'C3'])
         self.assertTrue("Group 'G1.G2' has the following cycles: [['C1', 'C2', 'C3']]" in
                         stream.getvalue())
         self.assertEqual(checks['out_of_order'], [('G1.G2',[('C2',['C3'])])])
