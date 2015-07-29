d8dd893313be840b33f3d7756303def7b864f28e
kmarsteller <Keith.G.Marsteller@nasa.gov>
2015-07-29 18:53:44 -0400

    adding units classes and commenting out some core

--- openmdao/core/__init__.py
+++ openmdao/core/__init__.py
@@ -6,8 +6,8 @@ from openmdao.core.system import System
 from openmdao.core.driver import Driver
 from openmdao.core.options import OptionsDictionary
 #do we need these?
-from openmdao.core.basic_impl import BasicImpl
-from openmdao.core.data_transfer import DataTransfer
-from openmdao.core.petsc_impl import PetscImpl
-from openmdao.core.relevance import Relevance
-from openmdao.core.vec_wrapper import VecWrapper
+# from openmdao.core.basic_impl import BasicImpl
+# from openmdao.core.data_transfer import DataTransfer
+# from openmdao.core.petsc_impl import PetscImpl
+# from openmdao.core.relevance import Relevance
+# from openmdao.core.vec_wrapper import VecWrapper
---
+++ openmdao/units/__init__.py
@@ -0,0 +1,4 @@
+from openmdao.units.units import PhysicalQuantity
+from openmdao.units.units import NumberDict
+from openmdao.units.units import UnitsOnlyPQ
+from openmdao.units.units import PhysicalUnit
