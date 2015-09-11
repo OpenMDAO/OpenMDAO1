3186360e24fdb21e7fbe0454a9688fa20b038bd5
Tristan Hearn <tristanhearn@gmail.com>
2015-09-11 11:11:54 -0400

    moved derivative test utility to /test/util.py

--- openmdao/test/util.py
+++ openmdao/test/util.py
@@ -5,6 +5,37 @@ from six import iteritems
 from math import isnan
 import numpy as np
 
+
+
+def problem_derivatives_test(unittest, problem, tol = 1e-5):
+    """
+    Runs partial derivates check on an OpenMDAO problem instance.
+    Asserts that forward and reverse derivatives are within a specified
+    relative tolerance.
+
+    unittest -> unittest.TestCase : unit test instance
+
+    problem  -> Problem : OpenMDAO problem instance to be tested
+
+    tol: float -> Float : tolerance for relative error in the derivative checks
+
+    """
+    partials = problem.check_partial_derivatives(out_stream=None)
+    for comp in partials:
+        
+        derivs = partials[comp]
+        for deriv in derivs.keys():
+            absol = derivs[deriv]['abs error'] 
+            err = derivs[deriv]['rel error']
+
+            if max(absol) > 0: # zero abs error implies rel error = nan
+                try:
+                    unittest.assertLessEqual(max(err), tol)
+                    # print "Deriv test passed:", comp, deriv, max(err) 
+                except AssertionError as e:
+                    print "Deriv test failed:", comp, deriv, max(err)   
+                    raise e
+
 def assert_rel_error(test_case, actual, desired, tolerance):
     """
     Determine that the relative error between `actual` and `desired`
