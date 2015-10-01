61ab03294c4698ac148e27c12b69ceeb4c1088d9
Tristan Hearn <tristanhearn@gmail.com>
2015-10-01 15:32:47 -0400

    switched default training data arg to None, test and set to list within method

--- openmdao/components/meta_model.py
+++ openmdao/components/meta_model.py
@@ -56,7 +56,7 @@ class MetaModel(Component):
 
         self._input_size = 0
 
-    def add_param(self, name, val=_NotSet, training_data=(), **kwargs):
+    def add_param(self, name, val=_NotSet, training_data=None, **kwargs):
         """ Add a `param` input to this component and a corresponding
         training parameter.
 
@@ -72,6 +72,9 @@ class MetaModel(Component):
             training data for this variable. Optional, can be set 
             by the problem later.
         """
+        if training_data is None:
+            training_data = list()
+
         super(MetaModel, self).add_param(name, val, **kwargs)
         super(MetaModel, self).add_param('train:'+name, val=training_data, pass_by_obj=True)
 
@@ -80,7 +83,7 @@ class MetaModel(Component):
         self._surrogate_param_names.append((name, input_size))
         self._input_size += input_size
 
-    def add_output(self, name, val=_NotSet, training_data=(), **kwargs):
+    def add_output(self, name, val=_NotSet, training_data=None, **kwargs):
         """ Add an output to this component and a corresponding
         training output.
 
@@ -97,6 +100,9 @@ class MetaModel(Component):
             training data for this variable. Optional, can be set 
             by the problem later.
         """
+        if training_data is None:
+            training_data = list()
+
         super(MetaModel, self).add_output(name, val, **kwargs)
         super(MetaModel, self).add_output('train:'+name, val=training_data, pass_by_obj=True)
 
