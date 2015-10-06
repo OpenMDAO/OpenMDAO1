5b3be06198d6a5e7e9a7cd470343b1ade4a95f23
Bret Naylor <naylor.b@gmail.com>
2015-10-06 16:20:41 -0400

    put back old dumat cleanup stuff in lings.solve

--- openmdao/solvers/ln_gauss_seidel.py
+++ openmdao/solvers/ln_gauss_seidel.py
@@ -77,8 +77,13 @@ class LinearGaussSeidel(LinearSolver):
         fwd = mode == 'fwd'
 
         system.clear_dparams()
-        for name in rhs_mat:
-            dumat[name].vec[:] = 0.0
+        # for name in rhs_mat:
+        #     dumat[name].vec[:] = 0.0
+        for names in system._relevance.vars_of_interest():
+            for name in names:
+                if name in dumat:
+                    dumat[name].vec[:] = 0.0
+        dumat[None].vec[:] = 0.0
 
         vois = rhs_mat.keys()
         # John starts with the following. It is not necessary, but
