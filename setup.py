4a1efdb574c15920940591d6a80d5ac3019baef8
kmarsteller <Keith.G.Marsteller@nasa.gov>
2015-11-04 09:59:33 -0500

    getting ready for 1.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='openmdao',
-      version='1.2.0',
+      version='1.3.0',
       description="OpenMDAO v1 framework infrastructure",
       long_description="""\
       """,
