38ba54ab08466d9c302a7197fb90e7a1bb858b1b
Tim Thomas <tthomas@metamorphsoftware.com>
2015-11-05 09:30:24 -0600

    Added Python 3.x compatibility, cleaned code.

--- openmdao/drivers/test/test_uniform_driver.py
+++ openmdao/drivers/test/test_uniform_driver.py
@@ -1,20 +1,18 @@
 import unittest
 from pprint import pformat
-from random import randint
 from types import GeneratorType
 
 import numpy as np
 
 from openmdao.api import IndepVarComp, Group, Problem
 from openmdao.test.paraboloid import Paraboloid
-from openmdao.test.sellar import SellarDerivatives, SellarStateConnection
-from openmdao.test.simple_comps import SimpleArrayComp, ArrayComp2D
-from openmdao.test.util import assert_rel_error
 
 from openmdao.drivers.uniform_driver import UniformDriver
 
 class TestUniformDriver(unittest.TestCase):
+
     def test_uniformDriver(self):
+
         prob = Problem()
         root = prob.root = Group()
 
@@ -26,11 +24,14 @@ class TestUniformDriver(unittest.TestCase):
         prob.driver.add_desvar('x', low=-10, high=10)
         prob.driver.add_desvar('y', low=-10, high=10)
         prob.driver.add_objective('f_xy')
+
+        prob.setup(check=False)
         runList = prob.driver._build_runlist()
-        #prob.setup(check=False)
-        #prob.run()
+        prob.run()
+        
         # Assert that the runList generated is of Generator Type
-        self.assertTrue((type(runList) == GeneratorType),"_build_runlist did not return a generator.")
+        self.assertTrue((type(runList) == GeneratorType),
+                        "_build_runlist did not return a generator.")
 
         # Assert the length is correct
         cases =list(runList)
@@ -38,10 +39,10 @@ class TestUniformDriver(unittest.TestCase):
         self.assertTrue(countRuns == 5, "Incorrect number of runs generated.")
 
         #assert that values are in range
-        inRange= False
+        inRange= True
         for value in cases:
-            if (-10< value['x'] < 10 and -10 <value['y'] <10):
-                inRange = True
+            if not (-10< value['x'] < 10 and -10 < value['y'] <10):
+                inRange = False
 
         self.assertTrue(inRange,"Not in range.")
 
