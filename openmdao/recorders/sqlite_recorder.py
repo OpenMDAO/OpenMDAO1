afdd33e15812226b469bbb6a629a5ad00ec173fc
Herb Schilling <hschilling@nasa.gov>
2015-11-16 13:32:13 -0500

    removed print

--- openmdao/recorders/sqlite_recorder.py
+++ openmdao/recorders/sqlite_recorder.py
@@ -93,6 +93,5 @@ class SqliteRecorder(BaseRecorder):
         print "QQQ SqliteRecorder.close", self._open_close_sqlitedict, self.out
         if self._open_close_sqlitedict:
             if self.out is not None:
-                print "QQQ SqliteRecorder.close inside if self.out is not none"
                 self.out.close()
                 self.out = None
