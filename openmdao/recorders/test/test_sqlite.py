7a0204f1a78c87fd04439132d5e1d4b960f501b8
Herb Schilling <hschilling@nasa.gov>
2015-11-16 09:17:19 -0500

    added even more print statements to debug windows error

--- openmdao/recorders/test/test_sqlite.py
+++ openmdao/recorders/test/test_sqlite.py
@@ -475,7 +475,8 @@ class TestSqliteRecorder(unittest.TestCase):
         prob = Problem()
         prob.root = ConvergeDiverge()
         prob.driver.add_recorder(self.recorder)
-        self.recorder.options['record_metadata'] = False
+        #self.recorder.options['record_metadata'] = False
+        self.recorder.options['record_metadata'] = True
         prob.setup(check=False)
         print "QQQ test_driver_doesnt_record_metadata: close recorder"
         self.recorder.close()
