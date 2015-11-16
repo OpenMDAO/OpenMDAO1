d734ec1beb803cee68fad018bfdb71cd54d982d5
Herb Schilling <hschilling@nasa.gov>
2015-11-16 13:38:44 -0500

    back to if self.out

--- openmdao/recorders/sqlite_recorder.py
+++ openmdao/recorders/sqlite_recorder.py
@@ -92,6 +92,7 @@ class SqliteRecorder(BaseRecorder):
 
         print "QQQ SqliteRecorder.close", self._open_close_sqlitedict, self.out
         if self._open_close_sqlitedict:
-            if self.out is not None:
+            # if self.out is not None:
+            if self.out :
                 self.out.close()
                 self.out = None
