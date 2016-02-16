ef445df47d73faa512a1b8e9b0f16a4457e95540
Rob Falck <rfalck@nasa.gov>
2016-02-16 16:49:25 -0500

    added test for undefined jacobian function, which should still return None

--- openmdao/core/test/test_empty_jacobian.py
+++ openmdao/core/test/test_empty_jacobian.py
@@ -1,10 +1,6 @@
 """ Test to verify that components without outputs will work as expected as
 long as their linearize method returns an empty dict {}.
 """
-
-
-__author__ = 'rfalck'
-
 import unittest
 
 from openmdao.api import Problem, Group, Component, ExecComp, IndepVarComp
@@ -15,10 +11,10 @@ class InputComp(Component):
 
     def __init__(self):
 
-        super(InputComp,self).__init__()
+        super(InputComp, self).__init__()
 
-        self.add_param(name='A',val=0.0)
-        self.add_param(name='B',val=0.0)
+        self.add_param(name="A", val=0.0)
+        self.add_param(name="B", val=0.0)
 
     def solve_nonlinear(self, params, unknowns, resids):
         pass
@@ -31,10 +27,10 @@ class InputComp2(Component):
 
     def __init__(self):
 
-        super(InputComp2,self).__init__()
+        super(InputComp2, self).__init__()
 
-        self.add_param(name='A',val=0.0)
-        self.add_param(name='B',val=0.0)
+        self.add_param(name="A", val=0.0)
+        self.add_param(name="B", val=0.0)
 
     def solve_nonlinear(self, params, unknowns, resids):
         pass
@@ -43,32 +39,56 @@ class InputComp2(Component):
         return None
 
 
+class InputComp3(Component):
+
+    def __init__(self):
+
+        super(InputComp3, self).__init__()
+
+        self.add_param(name="A", val=0.0)
+        self.add_param(name="B", val=0.0)
+
+    def solve_nonlinear(self, params, unknowns, resids):
+        pass
+
 
 class MyGroup(Group):
 
     def __init__(self):
-        super(MyGroup,self).__init__()
+        super(MyGroup, self).__init__()
 
-        self.add(name='input',system=InputComp())
+        self.add(name="input", system=InputComp())
 
-        self.add(name='exec',system=ExecComp('y=a**2+b**2',a=5.0,b=5.0))
+        self.add(name="exec", system=ExecComp("y=a**2+b**2", a=5.0, b=5.0))
 
-        self.connect('input.A','exec.a')
-        self.connect('input.B','exec.b')
-
+        self.connect("input.A", "exec.a")
+        self.connect("input.B", "exec.b")
 
 
 class MyGroup2(Group):
 
     def __init__(self):
-        super(MyGroup2,self).__init__()
+        super(MyGroup2, self).__init__()
 
-        self.add(name='input',system=InputComp2())
+        self.add(name="input", system=InputComp2())
 
-        self.add(name='exec',system=ExecComp('y=a**2+b**2',a=5.0,b=5.0))
+        self.add(name="exec", system=ExecComp("y=a**2+b**2", a=5.0, b=5.0))
 
-        self.connect('input.A','exec.a')
-        self.connect('input.B','exec.b')
+        self.connect("input.A", "exec.a")
+        self.connect("input.B", "exec.b")
+
+
+class MyGroup3(Group):
+
+    def __init__(self):
+        super(MyGroup3, self).__init__()
+
+        self.add(name="input", system=InputComp3())
+
+        self.add(name="exec", system=ExecComp("y=a**2+b**2", a=5.0, b=5.0))
+
+        self.connect("input.A", "exec.a")
+        self.connect("input.B", "exec.b")
 
 
 class TestEmptyJacobian(unittest.TestCase):
@@ -79,19 +99,21 @@ class TestEmptyJacobian(unittest.TestCase):
 
         root = prob.root
 
-        root.add(name='ivc_a',system=IndepVarComp(name='a',val=5.0),promotes=['a'])
-        root.add(name='ivc_b',system=IndepVarComp(name='b',val=10.0),promotes=['b'])
+        root.add(name="ivc_a", system=IndepVarComp(name="a", val=5.0),
+                 promotes=["a"])
+        root.add(name="ivc_b", system=IndepVarComp(name="b", val=10.0),
+                 promotes=["b"])
 
-        root.add(name='g',system=MyGroup())
+        root.add(name="g", system=MyGroup())
 
-        root.connect('a','g.input.A')
-        root.connect('b','g.input.B')
+        root.connect("a", "g.input.A")
+        root.connect("b", "g.input.B")
 
         prob.driver = ScipyOptimizer()
         prob.driver.options["disp"] = False
-        prob.driver.add_objective('g.exec.y')
-        prob.driver.add_desvar(name='a')
-        prob.driver.add_desvar(name='b')
+        prob.driver.add_objective("g.exec.y")
+        prob.driver.add_desvar(name="a")
+        prob.driver.add_desvar(name="b")
 
         prob.setup(check=False)
 
@@ -107,22 +129,53 @@ class TestEmptyJacobian(unittest.TestCase):
 
         root = prob.root
 
-        root.add(name='ivc_a',system=IndepVarComp(name='a',val=5.0),promotes=['a'])
-        root.add(name='ivc_b',system=IndepVarComp(name='b',val=10.0),promotes=['b'])
+        root.add(name="ivc_a", system=IndepVarComp(name="a", val=5.0),
+                 promotes=["a"])
+        root.add(name="ivc_b", system=IndepVarComp(name="b", val=10.0),
+                 promotes=["b"])
 
-        root.add(name='g',system=MyGroup2())
+        root.add(name="g", system=MyGroup2())
 
-        root.connect('a','g.input.A')
-        root.connect('b','g.input.B')
+        root.connect("a", "g.input.A")
+        root.connect("b", "g.input.B")
 
         prob.driver = ScipyOptimizer()
         prob.driver.options["disp"] = False
-        prob.driver.add_objective('g.exec.y')
-        prob.driver.add_desvar(name='a')
-        prob.driver.add_desvar(name='b')
+        prob.driver.add_objective("g.exec.y")
+        prob.driver.add_desvar(name="a")
+        prob.driver.add_desvar(name="b")
 
         prob.setup(check=False)
 
         with self.assertRaises(ValueError):
             prob.run()
 
+    def test_undefined_jacobian(self):
+
+        prob = Problem(root=Group())
+
+        root = prob.root
+
+        root.add(name="ivc_a", system=IndepVarComp(name="a", val=5.0),
+                 promotes=["a"])
+        root.add(name="ivc_b", system=IndepVarComp(name="b", val=10.0),
+                 promotes=["b"])
+
+        root.add(name="g", system=MyGroup3())
+
+        root.connect("a", "g.input.A")
+        root.connect("b", "g.input.B")
+
+        prob.driver = ScipyOptimizer()
+        prob.driver.options["disp"] = False
+        prob.driver.add_objective("g.exec.y")
+        prob.driver.add_desvar(name="a")
+        prob.driver.add_desvar(name="b")
+
+        prob.setup(check=False)
+
+        with self.assertRaises(ValueError):
+            prob.run()
+
+if __name__ == "__main__":
+    unittest.main()
