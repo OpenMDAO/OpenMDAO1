59ba29410ec607d3340084d89169a972c4c21f67
Rob Falck <rfalck@nasa.gov>
2016-02-29 11:12:12 -0500

    tests to verfiy that check_partial_derivs and check_total_derivs invoke run_once when necessary

--- openmdao/core/test/test_check_derivatives.py
+++ openmdao/core/test/test_check_derivatives.py
@@ -1,6 +1,6 @@
 """ Testing for Problem.check_partial_derivatives and check_total_derivatives."""
 
-from six import iteritems
+from six import iteritems, StringIO
 import unittest
 
 import numpy as np
@@ -406,6 +406,94 @@ class TestProblemCheckTotals(unittest.TestCase):
         for key, val in iteritems(data):
             assert_rel_error(self, val['abs error'][1], 0.0, 1e-5)
             assert_rel_error(self, val['rel error'][1], 0.0, 1e-5)
+
+    def test_check_partials_calls_run_once(self):
+        prob = Problem()
+        root = prob.root = Group()
+        root.add('p1', IndepVarComp('x', 1.0), promotes=['*'])
+        root.add('p2', IndepVarComp('y', 1.0), promotes=['*'])
+        root.add('comp', Paraboloid(), promotes=['*'])
+
+        prob.driver.add_desvar('x')
+        prob.driver.add_desvar('y')
+        prob.driver.add_objective('f_xy')
+
+        prob.setup(check=False)
+
+        prob['x'] = 5.0
+        prob['y'] = 2.0
+
+        iostream = StringIO()
+
+        data = prob.check_partial_derivatives(out_stream=iostream)
+
+        self.assertAlmostEqual(first=prob["f_xy"],
+                               second= (prob['x']-3.0)**2 \
+                                       + prob['x']*prob['y'] \
+                                       + (prob['y']+4.0)**2 - 3.0,
+                               places=5,
+                               msg="check partial derivatives did not call"
+                                   "run_once on the driver as expected.")
+
+        self.assertEqual(first=iostream.getvalue()[:39],
+                         second="Executing model to populate unknowns...",
+                         msg="check partial derivatives failed to run driver once")
+
+        self.assertAlmostEqual(first=data["comp"]["f_xy","x"]["J_fd"],
+                               second=2.0*prob["x"] - 6.0 + prob["y"],
+                               places=5,
+                               msg="Paraboloid derivative incorrect at the "
+                                   "expected design point.")
+
+        self.assertAlmostEqual(first=data["comp"]["f_xy","y"]["J_fd"],
+                               second=2.0*prob["y"] + 8.0 + prob["x"],
+                               places=5,
+                               msg="Paraboloid derivative incorrect at the "
+                                   "expected design point.")
+
+    def test_check_totals_calls_run_once(self):
+        prob = Problem()
+        root = prob.root = Group()
+        root.add('p1', IndepVarComp('x', 1.0), promotes=['*'])
+        root.add('p2', IndepVarComp('y', 1.0), promotes=['*'])
+        root.add('comp', Paraboloid(), promotes=['*'])
+
+        prob.driver.add_desvar('x')
+        prob.driver.add_desvar('y')
+        prob.driver.add_objective('f_xy')
+
+        prob.setup(check=False)
+
+        prob['x'] = 5.0
+        prob['y'] = 2.0
+
+        iostream = StringIO()
+
+        data = prob.check_total_derivatives(out_stream=iostream)
+
+        self.assertAlmostEqual(first=prob["f_xy"],
+                               second= (prob['x']-3.0)**2 \
+                                       + prob['x']*prob['y'] \
+                                       + (prob['y']+4.0)**2 - 3.0,
+                               places=5,
+                               msg="check partial derivatives did not call"
+                                   "run_once on the driver as expected.")
+
+        self.assertEqual(first=iostream.getvalue()[:39],
+                         second="Executing model to populate unknowns...",
+                         msg="check partial derivatives failed to run driver once")
+
+        self.assertAlmostEqual(first=data["f_xy","x"]["J_fd"],
+                               second=2.0*prob["x"] - 6.0 + prob["y"],
+                               places=5,
+                               msg="Paraboloid derivative incorrect at the "
+                                   "expected design point.")
+
+        self.assertAlmostEqual(first=data["f_xy","y"]["J_fd"],
+                               second=2.0*prob["y"] + 8.0 + prob["x"],
+                               places=5,
+                               msg="Paraboloid derivative incorrect at the "
+                                   "expected design point.")
 
 if __name__ == "__main__":
     unittest.main()
