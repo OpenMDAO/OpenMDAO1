d1e5a86f2391b0d461a59ab8807172a875cc291b
Bret Naylor <naylor.b@gmail.com>
2016-08-04 16:07:43 -0400

    fix for mpi with doe subproblem

--- openmdao/core/test/test_subproblem.py
+++ openmdao/core/test/test_subproblem.py
@@ -83,58 +83,6 @@ class ErrProb(Problem):
         raise RuntimeError("Houston, we have a problem.")
 
 
-class UQTestDriver(CaseDriver):
-    def __init__(self, nsamples, num_par_doe=1, load_balance=False):
-        super(UQTestDriver, self).__init__(num_par_doe=num_par_doe,
-                                             load_balance=load_balance)
-        self.nsamples = nsamples
-        self.std_devs = {}
-        self.dist = {}
-
-    def add_desvar(self, name, **kwargs):
-        if 'std_dev' in kwargs:
-            self.std_devs[name] = kwargs.pop('std_dev')
-        super(UQTestDriver, self).add_desvar(name, **kwargs)
-
-    def run(self, problem):
-        if not self.dist:
-            for dv in self._desvars:
-                self.dist[dv] = np.random.normal(0.0, 1.0, self.nsamples) # std normal dist
-
-        self.cases = []
-        for i in range(self.nsamples):
-            case = []
-            for dv in self._desvars:
-                dval = problem[dv]
-                if dv in self.std_devs:
-                    dval += self.dist[dv][i]*self.std_devs[dv]
-                case.append((dv, dval))
-
-            #print("case: ",case)
-            self.cases.append(case)
-
-        super(UQTestDriver, self).run(problem)
-
-        uncertain_outputs = {}
-
-        # collect the responses and find the mean
-        for responses, _, _ in self.get_responses():
-            for name, val in responses:
-                if name not in uncertain_outputs:
-                    uncertain_outputs[name] = data = [0.0, 0.0]
-                else:
-                    data = uncertain_outputs[name]
-                data[0] += 1
-                data[1] += val
-
-        # now, set response values in unknowns to the mean value of our
-        # uncertain outputs
-        for name in uncertain_outputs:
-            if name in self.root.unknowns:
-                data = uncertain_outputs[name]
-                self.root.unknowns[name] = data[1]/data[0]
-
-
 
 class TestSubProblem(unittest.TestCase):
 
@@ -412,61 +360,109 @@ class TestSubProblem(unittest.TestCase):
         assert_rel_error(self, prob['obj'], 3.1833940, 1e-5)
 
 
-# FIXME: there are issues with running subproblems under MPI!!!!
-# class TestSubProblemMPI(MPITestCase):
-#     N_PROCS = 8
-#
-#     def test_opt_over_doe_uq(self):
-#         np.random.seed(42)
-#
-#         prob = Problem(impl=impl, root=Group())
-#         prob.root.deriv_options['type'] = 'fd'
-#
-#         subprob = Problem(impl=impl, root=SellarNoDerivatives())
-#         subprob.root.deriv_options['type'] = 'fd'
-#         subprob.driver = UQTestDriver(nsamples=80, num_par_doe=8)
-#         subprob.driver.add_desvar('z', std_dev=1e-2)
-#         subprob.driver.add_desvar('x', std_dev=1e-2)
-#         subprob.driver.add_response('obj')
-#         subprob.driver.add_response('con1')
-#         subprob.driver.add_response('con2')
-#
-#         #subprob.driver.recorders.append(SqliteRecorder("subsellar.db"))
-#
-#         prob.root.add("indeps", IndepVarComp([('x', 1.0),
-#                                               ('z', np.array([5.0, 2.0]))]),
-#                       promotes=['x', 'z'])
-#         prob.root.add("sub", SubProblem(subprob, params=['z','x'],
-#                                                  unknowns=['obj', 'con1', 'con2']))
-#
-#         prob.root.connect('x', 'sub.x')
-#         prob.root.connect('z', 'sub.z')
-#
-#         # top level driver setup
-#         prob.driver = ScipyOptimizer()
-#         prob.driver.options['optimizer'] = 'SLSQP'
-#         prob.driver.options['tol'] = 1.0e-8
-#         #prob.driver.options['disp'] = False
-#
-#         prob.driver.add_desvar('z', lower=np.array([-10.0,  0.0]),
-#                                     upper=np.array([ 10.0, 10.0]))
-#         prob.driver.add_desvar('x', lower=0.0, upper=10.0)
-#
-#         prob.driver.add_objective('sub.obj')
-#         prob.driver.add_constraint('sub.con1', upper=0.0)
-#         prob.driver.add_constraint('sub.con2', upper=0.0)
-#
-#         #prob.driver.recorders.append(SqliteRecorder("sellar.db"))
-#
-#         prob.setup(check=False)
-#
-#         prob.run()
-#
-#         assert_rel_error(self, prob['sub.obj'], 3.1833940, 1e-5)
-#         assert_rel_error(self, prob['z'][0], 1.977639, 1e-5)
-#         assert_rel_error(self, prob['z'][1], 0.0, 1e-5)
-#         assert_rel_error(self, prob['x'], 0.0, 1e-5)
-#
+
+class UQTestDriver(CaseDriver):
+    def __init__(self, nsamples, num_par_doe=1, load_balance=False):
+        super(UQTestDriver, self).__init__(num_par_doe=num_par_doe,
+                                             load_balance=load_balance)
+        self.nsamples = nsamples
+        self.dist = {}
+
+    def add_desvar(self, name, **kwargs):
+        if 'std_dev' in kwargs:
+            self.dist[name] = kwargs.pop('std_dev')*np.random.normal(0.0, 1.0,
+                                                                  self.nsamples)
+        super(UQTestDriver, self).add_desvar(name, **kwargs)
+
+    def run(self, problem):
+        self.cases = []
+        for i in range(self.nsamples):
+            case = []
+            for dv in self._desvars:
+                dval = problem[dv]
+                if dv in self.dist:
+                    dval += self.dist[dv][i]
+                case.append((dv, dval))
+
+            #print("case: ",case)
+            self.cases.append(case)
+
+        super(UQTestDriver, self).run(problem)
+
+        uncertain_outputs = {}
+
+        # collect the responses and find the mean
+        for responses, _, _ in self.get_all_responses():
+            for name, val in responses:
+                if name not in uncertain_outputs:
+                    uncertain_outputs[name] = data = [0.0, 0.0]
+                else:
+                    data = uncertain_outputs[name]
+                data[0] += 1.
+                data[1] += val
+
+        # now, set response values in unknowns to the mean value of our
+        # uncertain outputs
+        for name in uncertain_outputs:
+            if name in self.root.unknowns:
+                data = uncertain_outputs[name]
+                self.root.unknowns[name] = data[1]/data[0]
+
+
+class TestSubProblemMPI(MPITestCase):
+    N_PROCS = 5
+
+    def test_opt_over_doe_uq(self):
+        np.random.seed(42)
+
+        prob = Problem(impl=impl, root=Group())
+        prob.root.deriv_options['type'] = 'fd'
+
+        subprob = Problem(impl=impl, root=SellarNoDerivatives())
+        subprob.root.deriv_options['type'] = 'fd'
+        subprob.driver = UQTestDriver(nsamples=500, num_par_doe=self.N_PROCS)
+        subprob.driver.add_desvar('z', std_dev=1e-2)
+        subprob.driver.add_desvar('x', std_dev=1e-2)
+        subprob.driver.add_response('obj')
+        subprob.driver.add_response('con1')
+        subprob.driver.add_response('con2')
+
+        #subprob.driver.recorders.append(SqliteRecorder("subsellar.db"))
+
+        prob.root.add("indeps", IndepVarComp([('x', 1.0),
+                                              ('z', np.array([5.0, 2.0]))]),
+                      promotes=['x', 'z'])
+        prob.root.add("sub", SubProblem(subprob, params=['z','x'],
+                                                 unknowns=['obj', 'con1', 'con2']))
+
+        prob.root.connect('x', 'sub.x')
+        prob.root.connect('z', 'sub.z')
+
+        # top level driver setup
+        prob.driver = ScipyOptimizer()
+        prob.driver.options['optimizer'] = 'SLSQP'
+        prob.driver.options['tol'] = 1.0e-8
+        #prob.driver.options['disp'] = False
+
+        prob.driver.add_desvar('z', lower=np.array([-10.0,  0.0]),
+                                    upper=np.array([ 10.0, 10.0]))
+        prob.driver.add_desvar('x', lower=0.0, upper=10.0)
+
+        prob.driver.add_objective('sub.obj')
+        prob.driver.add_constraint('sub.con1', upper=0.0)
+        prob.driver.add_constraint('sub.con2', upper=0.0)
+
+        #prob.driver.recorders.append(SqliteRecorder("sellar.db"))
+
+        prob.setup(check=False)
+
+        prob.run()
+
+        assert_rel_error(self, prob['sub.obj'], 3.1833940, 1e-5)
+        assert_rel_error(self, prob['z'][0], 1.977639, 1e-5)
+        assert_rel_error(self, prob['z'][1], 0.0, 1e-5)
+        assert_rel_error(self, prob['x'], 0.0, 1e-5)
+
 
 if __name__ == "__main__":
     unittest.main()
