8895b0ae21a577da33cf9f50df9a0991af12581c
Keith Marsteller <Keith.G.Marsteller@nasa.gov>
2016-08-04 18:16:49 -0400

    getting ready to work with readthedocs

--- openmdao/docs/conf.py
+++ openmdao/docs/conf.py
@@ -20,11 +20,14 @@ sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 from openmdao.api import Problem
 #from examples.sellar_state_MDF_optimize import SellarStateConnection
-""" Optimize the Sellar problem using SLSQP."""
+""" Optimize the Sellar problem using SLSQP. This version elminates the cycle
+and replaces it with an implicit component."""
 
 import numpy as np
 
-from openmdao.api import Component, Group, IndepVarComp, ExecComp, NLGaussSeidel, ScipyGMRES
+from openmdao.api import Component, Group, IndepVarComp, \
+    ExecComp, Newton, ScipyGMRES
+
 
 class SellarDis1(Component):
     """Component containing Discipline 1."""
@@ -106,50 +109,84 @@ class SellarDis2(Component):
         return J
 
 
-class SellarDerivatives(Group):
+class StateConnection(Component):
+    """ Define connection with an explicit equation"""
+
+    def __init__(self):
+        super(StateConnection, self).__init__()
+
+        # Inputs
+        self.add_param('y2_actual', 1.0)
+
+        # States
+        self.add_state('y2_command', val=1.0)
+
+    def apply_nonlinear(self, params, unknowns, resids):
+        """ Don't solve; just calculate the residual."""
+
+        y2_actual = params['y2_actual']
+        y2_command = unknowns['y2_command']
+
+        resids['y2_command'] = y2_actual - y2_command
+
+    def solve_nonlinear(self, params, unknowns, resids):
+        """ This is a dummy comp that doesn't modify its state."""
+        pass
+
+    def linearize(self, params, unknowns, resids):
+        """Analytical derivatives."""
+
+        J = {}
+
+        # State equation
+        J[('y2_command', 'y2_command')] = -1.0
+        J[('y2_command', 'y2_actual')] = 1.0
+
+        return J
+
+
+class SellarStateConnection(Group):
     """ Group containing the Sellar MDA. This version uses the disciplines
     with derivatives."""
 
     def __init__(self):
-        super(SellarDerivatives, self).__init__()
+        super(SellarStateConnection, self).__init__()
 
         self.add('px', IndepVarComp('x', 1.0), promotes=['x'])
         self.add('pz', IndepVarComp('z', np.array([5.0, 2.0])), promotes=['z'])
 
-        self.add('d1', SellarDis1(), promotes=['x', 'z', 'y1', 'y2'])
-        self.add('d2', SellarDis2(), promotes=['z', 'y1', 'y2'])
+        self.add('state_eq', StateConnection())
+        self.add('d1', SellarDis1(), promotes=['x', 'z', 'y1'])
+        self.add('d2', SellarDis2(), promotes=['z', 'y1'])
+
+        self.connect('state_eq.y2_command', 'd1.y2')
+        self.connect('d2.y2', 'state_eq.y2_actual')
 
         self.add('obj_cmp', ExecComp('obj = x**2 + z[1] + y1 + exp(-y2)',
-                                     z=np.array([0.0, 0.0]) ),
-                 promotes=['obj', 'x', 'z', 'y1', 'y2'])
+                                     z=np.array([0.0, 0.0])),
+                  promotes=['x', 'z', 'y1', 'obj'])
+        self.connect('d2.y2', 'obj_cmp.y2')
 
         self.add('con_cmp1', ExecComp('con1 = 3.16 - y1'), promotes=['con1', 'y1'])
-        self.add('con_cmp2', ExecComp('con2 = y2 - 24.0'), promotes=['con2', 'y2'])
+        self.add('con_cmp2', ExecComp('con2 = y2 - 24.0'), promotes=['con2'])
+        self.connect('d2.y2', 'con_cmp2.y2')
 
-        self.nl_solver = NLGaussSeidel()
-        self.nl_solver.options['atol'] = 1.0e-12
+        self.nl_solver = Newton()
         self.ln_solver = ScipyGMRES()
 
 
 if __name__ == '__main__':
-
-    from openmdao.api import Problem, ScipyOptimizer, SqliteRecorder
-
     # Setup and run the model.
 
+    from openmdao.core.problem import Problem
+    from openmdao.drivers.scipy_optimizer import ScipyOptimizer
+
     top = Problem()
-    top.root = SellarDerivatives()
+    top.root = SellarStateConnection()
 
     top.driver = ScipyOptimizer()
     top.driver.options['optimizer'] = 'SLSQP'
     top.driver.options['tol'] = 1.0e-8
-
-    # This is for testing with SNOPT via pyOptSparse
-    '''
-    from openmdao.api import pyOptSparseDriver
-    top.driver = pyOptSparseDriver()
-    top.driver.options['optimizer'] = 'SNOPT'
-    '''
 
     top.driver.add_desvar('z', lower=np.array([-10.0, 0.0]),
                          upper=np.array([10.0, 10.0]))
@@ -158,9 +195,6 @@ if __name__ == '__main__':
     top.driver.add_objective('obj')
     top.driver.add_constraint('con1', upper=0.0)
     top.driver.add_constraint('con2', upper=0.0)
-    rec = SqliteRecorder('sellar_snopt.db')
-    top.driver.add_recorder(rec)
-    rec.options['record_derivs'] = True
 
     top.setup()
     top.run()
@@ -169,7 +203,7 @@ if __name__ == '__main__':
     print( "Minimum found at (%f, %f, %f)" % (top['z'][0],
                                              top['z'][1],
                                              top['x']))
-    print("Coupling vars: %f, %f" % (top['y1'], top['y2']))
+    print("Coupling vars: %f, %f" % (top['y1'], top['d2.y2']))
     print("Minimum objective: ", top['obj'])
 
 from openmdao.api import view_tree
