1e52b7608f262b5d63d330fb4a1a7036d5d76a3f
Keith Marsteller <Keith.G.Marsteller@nasa.gov>
2016-08-04 16:59:35 -0400

    getting ready to work with readthedocs

--- openmdao/docs/conf.py
+++ openmdao/docs/conf.py
@@ -18,6 +18,19 @@ import os
 MOCK_MODULES = ['h5py', 'petsc4py', 'mpi4py', 'pyoptsparse']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
+from openmdao.api import Problem
+from examples.sellar_state_MDF_optimize import SellarStateConnection
+from openmdao.api import view_tree
+
+top = Problem()
+top.root = SellarStateConnection()
+
+top.setup(check=False)
+current_dir = os.path.dirname(os.path.abspath(__file__))
+view_tree(top, show_browser=False, offline=False, outfile=os.path.join( 'usr-guide/tutorials', 'n2_sellar_state.html'))
+
+
+
 #------------------------begin monkeypatch-----------------------
 #monkeypatch to make our docs say "Args" instead of "Parameters"
 from numpydoc.docscrape_sphinx import SphinxDocString
