8d66a68e8ae742d01079a25f0bff889cec832f23
Keith Marsteller <Keith.G.Marsteller@nasa.gov>
2016-11-16 10:49:11 -0500

    updating versions for 1.7.3 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='openmdao',
-      version='1.7.2',
+      version='1.7.3',
       description="OpenMDAO v1 framework infrastructure",
       long_description="""\
       """,
@@ -24,7 +24,7 @@ setup(name='openmdao',
       author='OpenMDAO Team',
       author_email='openmdao@openmdao.org',
       url='http://openmdao.org',
-      download_url='http://github.com/OpenMDAO/OpenMDAO/tarball/1.7.2',
+      download_url='http://github.com/OpenMDAO/OpenMDAO/tarball/1.7.3',
       license='Apache License, Version 2.0',
       packages=[
           'openmdao',
