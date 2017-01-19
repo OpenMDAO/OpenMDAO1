2013a383500954f528e7911016dd0a2ad33d8c2b
Rob Falck <rfalck@nasa.gov>
2017-01-19 15:45:22 -0500

    fixed missing square root in hohmann example

--- openmdao/examples/test/test_examples.py
+++ openmdao/examples/test/test_examples.py
@@ -599,9 +599,9 @@ class TestExamples(unittest.TestCase):
 
         prob.run()
 
-        self.assertAlmostEqual(prob['delta_v'], 9.00599000737, places=7)
-        self.assertAlmostEqual(prob['dinc1'], 1.6672269916, places=7)
-        self.assertAlmostEqual(prob['dinc2'], 26.8327730084, places=7)
+        self.assertAlmostEqual(prob['delta_v'], 4.19629667348, places=7)
+        self.assertAlmostEqual(prob['dinc1'], 2.222172849, places=7)
+        self.assertAlmostEqual(prob['dinc2'], 26.277827151, places=7)
 
 if __name__ == "__main__":
     unittest.main()
